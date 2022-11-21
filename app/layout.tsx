55ca0addf19243c6884e2fc3ddb4b207256d7c9d
Arunava <this.is.arunava.b@gmail.com>
2022-11-21 20:40:12 +0530

    DON'T REMOVE HTML AND BODY FROM ROOT LAYOUT

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
-import Head from './head';
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <body>{children}</body>;
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
 }
