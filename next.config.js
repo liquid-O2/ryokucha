3aa34982c4892b189fbacfb99191579b95f2e2a8
Arunava <this.is.arunava.b@gmail.com>
2022-11-23 16:15:08 +0530

    a

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { appDir: true },
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
 
 module.exports = nextConfig
