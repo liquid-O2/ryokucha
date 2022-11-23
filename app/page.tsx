6afa4714f4da4720cc3cbaf3413711a9c10d8f6e
Arunava <this.is.arunava.b@gmail.com>
2022-11-23 19:02:10 +0530

    reverted changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useRouter } from 'next/navigation'
-import { useLayoutEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/globals.css'
 
 export default function App() {
@@ -24,7 +24,7 @@ export default function App() {
 
   //
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     const localState = localStorage.getItem('loggedIn')
     if (!localState) {
       router.push('/login')
@@ -44,14 +44,16 @@ export default function App() {
 
   return (
     <>
-      <div className='wrapper flex flex-col justify-center items-center h-screen w-screen text-neutral-200 '>
-        <h1 className='font-sans text-4xl font-bold'>Welcome Home</h1>
-        <button
-          className='bg-neutral-200 px-4 py-4 text-lg rounded-lg text-neutral-900 shadow-lg shadow-neutral-200/20 font-bold logout mt-6 hover:bg-neutral-300'
-          onClick={() => handleSignOut()}>
-          Log Out
-        </button>
-      </div>
+      {isLoggedIn && (
+        <div className='wrapper flex flex-col justify-center items-center h-screen w-screen text-neutral-200 '>
+          <h1 className='font-sans text-4xl font-bold'>Welcome Home</h1>
+          <button
+            className='bg-neutral-200 px-4 py-4 text-lg rounded-lg text-neutral-900 shadow-lg shadow-neutral-200/20 font-bold logout mt-6 hover:bg-neutral-300'
+            onClick={() => handleSignOut()}>
+            Log Out
+          </button>
+        </div>
+      )}
     </>
   )
 }
