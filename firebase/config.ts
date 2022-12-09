e640fba272078f4603a596d629b8b1b150ab0a53
arunava(liquido2) <this.is.arunava.b@gmail.com>
2022-12-09 19:10:59 +0530

    update: firebase api key

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth, connectAuthEmulator } from 'firebase/auth'
 import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: `${process.env.FIREBASE_API_KEY}`,
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: 'complexapp-5d19d.firebaseapp.com',
   projectId: 'complexapp-5d19d',
   storageBucket: 'complexapp-5d19d.appspot.com',
   messagingSenderId: '703550922847',
-  appId: `${process.env.FIREBASE_APP_ID}`,
+  appId: process.env.FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
