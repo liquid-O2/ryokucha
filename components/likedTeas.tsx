fd634311eff107e38481ffa732ae29beca9250d1
arunava(liquido2) <this.is.arunava.b@gmail.com>
2022-12-11 18:46:39 +0530

    cleanup: reduce unecessary files & optimise fetch

---
+++ components/likedTeas.tsx
@@ -0,0 +1,29 @@
+import { collection, getDocs, limit, query, where } from 'firebase/firestore'
+import { db } from '../firebase/config'
+import Carousel from './carousel'
+
+export type Teas = {
+  Name: string
+  Id: string
+  Attributes: Array<string>
+  Image: string
+  Price: string
+}
+
+const fetchTeas = async () => {
+  const q = query(collection(db, 'teas'), where('Featured', '==', true), limit(6))
+  const data = await getDocs(q)
+  const teas = data.docs.map((doc) => ({ ...doc.data(), Id: doc.id }))
+  return teas as Teas[]
+}
+
+export const LikedTeas = async () => {
+  let teas = await fetchTeas()
+  return (
+    <>
+      <div className='w-screen'>
+        <Carousel teas={teas} />
+      </div>
+    </>
+  )
+}
