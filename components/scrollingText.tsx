766717740d30347f0a04ae346a35c0c3bd0a0fca
arunava(liquido2) <this.is.arunava.b@gmail.com>
2022-12-13 23:58:06 +0530

    feat: new description section using css grid

--- components/scrollingText.tsx
+++ components/scrollingText.tsx
@@ -1,8 +1,8 @@
 export const ScrollingText = () => {
   return (
     <>
-      <div className='scrolling-div mb-24 w-screen overflow-hidden'>
-        <p className=' text-3xl font-serif font-bold tracking-tight overscroll-x-none whitespace-nowrap'>{`Helps in weight loss  –  緑茶  –  Rich, umami filled taste  –  緑茶  – Only from the best farms in Japan   – 緑茶  – Helps in weight loss  –  緑茶  –  Rich, umami filled taste  –  緑茶  – Only from the best farms in Japan   – 緑茶  –`}</p>
+      <div className='scrolling-div mb-32 w-screen overflow-hidden'>
+        <p className=' text-3xl font-serif font-bold tracking-tight overscroll-x-none whitespace-nowrap'>{`Authentic Japanese Green Tea  –  緑茶  –  Rich, umami filled taste  –  緑茶  – Only from the best farms in Japan   – 緑茶  – Authentic Japanese Green Tea  –  緑茶  –  Rich, umami filled taste  –  緑茶  – Only from the best farms in Japan   – 緑茶  –`}</p>
       </div>
     </>
   )
