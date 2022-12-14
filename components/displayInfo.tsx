dea3950eb3fb74de18e01824d808eac6fbe19137
arunava(liquido2) <this.is.arunava.b@gmail.com>
2022-12-14 21:05:47 +0530

    fix: framer initial package size

--- components/displayInfo.tsx
+++ components/displayInfo.tsx
@@ -4,96 +4,98 @@ import Image from 'next/image'
 import col1 from '../public/col1.jpg'
 import col2 from '../public/col2.jpg'
 import col3 from '../public/col3.jpg'
-import { useScroll, useTransform, motion } from 'framer-motion'
+import { useScroll, useTransform, m, domAnimation, LazyMotion } from 'framer-motion'
 const DisplayInfo = () => {
   const { scrollYProgress } = useScroll()
   const y = useTransform(scrollYProgress, [0.5, 1], ['0%', '10%'])
   return (
-    <Container>
-      <div className='grid grid-cols-[1] grid-flow-row  min-[880px]:grid-cols-3 third-section gap-y-4  gap-x-6 min-[880px]:gap-y-10 mb-24 md:mb-32'>
-        <div className='overflow-hidden  flex w-[75%] min-[880px]:w-auto md:items-end'>
-          <motion.p
-            initial={{ opacity: 0, y: '35%' }}
-            whileInView={{ opacity: 1, y: '0%' }}
-            transition={{ duration: 1 }}
-            viewport={{ once: true }}
-            className='font-bold text-3xl '>
-            The best green tea from the
-            <br className='hidden min-[1328px]:block' />
-            {` best farmers in Japan`}
-          </motion.p>
+    <LazyMotion features={domAnimation}>
+      <Container>
+        <div className='grid grid-cols-[1] grid-flow-row  min-[880px]:grid-cols-3 third-section gap-y-4  gap-x-6 min-[880px]:gap-y-10 mb-24 md:mb-32'>
+          <div className='overflow-hidden  flex w-[75%] min-[880px]:w-auto md:items-end'>
+            <m.p
+              initial={{ opacity: 0, y: '35%' }}
+              whileInView={{ opacity: 1, y: '0%' }}
+              transition={{ duration: 1 }}
+              viewport={{ once: true }}
+              className='font-bold text-3xl '>
+              The best green tea from the
+              <br className='hidden min-[1328px]:block' />
+              {` best farmers in Japan`}
+            </m.p>
+          </div>
+          <div className='overflow-hidden min-[880px]:col-span-2 flex justify-end items-end'>
+            <m.p
+              initial={{ opacity: 0, y: '35%' }}
+              whileInView={{ opacity: 1, y: '0%' }}
+              transition={{ duration: 1 }}
+              viewport={{ once: true }}
+              className=' text-xl char flex justify-end min-[880px]:pl-[10rem]  mb-6 min-[880px]:mb-0'>
+              Ryokucha sources the highest quality green teas from Japan and delivers them directly to you.
+              <br className='hidden min-[1664px]:block' /> Our teas are organically farmed and provide a true taste of
+              Japan.
+            </m.p>
+          </div>
+          <div className='relative h-[400px] min-[880px]:h-[500px] overflow-hidden rounded-3xl md:rounded-[3rem]'>
+            <m.div
+              initial={{ height: '100%' }}
+              whileInView={{ height: '0%' }}
+              transition={{ duration: 1, delay: 0.2 }}
+              viewport={{ once: true }}
+              className='absolute top-0 w-full h-full z-20 bg-primary'></m.div>
+            <m.div
+              initial={{ scale: 1.2, y: 0 }}
+              style={{ scale: 1.2, y: y }}
+              className={'relative w-full h-full overflow-hidden '}>
+              <Image
+                className='w-full h-full object-cover '
+                src={col1}
+                alt={'a small teacup with green tea'}
+                quality={10}
+              />
+            </m.div>
+          </div>
+          <div className='relative h-[400px] min-[880px]:h-[500px] overflow-hidden rounded-3xl md:rounded-[3rem]'>
+            <m.div
+              initial={{ height: '100%' }}
+              whileInView={{ height: '0%' }}
+              transition={{ duration: 1, delay: 0.2 }}
+              viewport={{ once: true }}
+              className='absolute top-0 w-full h-full z-20 bg-primary'></m.div>
+            <m.div
+              initial={{ scale: 1.2, y: 0 }}
+              style={{ scale: 1.2, y: y }}
+              className={'relative w-full h-full overflow-hidden '}>
+              <Image
+                className='w-full h-full object-cover '
+                src={col2}
+                alt={'a small porcelain dish with loose green tea leaves and some small cups with green tea in it'}
+                quality={10}
+              />
+            </m.div>
+          </div>
+          <div className='relative h-[400px] min-[880px]:h-[500px] overflow-hidden rounded-3xl md:rounded-[3rem]'>
+            <m.div
+              initial={{ height: '100%' }}
+              whileInView={{ height: '0%' }}
+              transition={{ duration: 1, delay: 0.2 }}
+              viewport={{ once: true }}
+              className='absolute top-0 w-full h-full z-20 bg-primary'></m.div>
+            <m.div
+              initial={{ scale: 1.2, y: 0 }}
+              style={{ scale: 1.2, y: y }}
+              className={'relative w-full h-full overflow-hidden '}>
+              <Image
+                className='w-full h-full object-cover '
+                src={col3}
+                alt={'a small dish with some water and some green leaves on top'}
+                quality={10}
+              />
+            </m.div>
+          </div>
         </div>
-        <div className='overflow-hidden min-[880px]:col-span-2 flex justify-end items-end'>
-          <motion.p
-            initial={{ opacity: 0, y: '35%' }}
-            whileInView={{ opacity: 1, y: '0%' }}
-            transition={{ duration: 1 }}
-            viewport={{ once: true }}
-            className=' text-xl char flex justify-end min-[880px]:pl-[10rem]  mb-6 min-[880px]:mb-0'>
-            Ryokucha sources the highest quality green teas from Japan and delivers them directly to you.
-            <br className='hidden min-[1664px]:block' /> Our teas are organically farmed and provide a true taste of
-            Japan.
-          </motion.p>
-        </div>
-        <div className='relative h-[400px] min-[880px]:h-[500px] overflow-hidden rounded-3xl md:rounded-[3rem]'>
-          <motion.div
-            initial={{ height: '100%' }}
-            whileInView={{ height: '0%' }}
-            transition={{ duration: 1, delay: 0.2 }}
-            viewport={{ once: true }}
-            className='absolute top-0 w-full h-full z-20 bg-primary'></motion.div>
-          <motion.div
-            initial={{ scale: 1.2, y: 0 }}
-            style={{ scale: 1.2, y: y }}
-            className={'relative w-full h-full overflow-hidden '}>
-            <Image
-              className='w-full h-full object-cover '
-              src={col1}
-              alt={'a small teacup with green tea'}
-              quality={10}
-            />
-          </motion.div>
-        </div>
-        <div className='relative h-[400px] min-[880px]:h-[500px] overflow-hidden rounded-3xl md:rounded-[3rem]'>
-          <motion.div
-            initial={{ height: '100%' }}
-            whileInView={{ height: '0%' }}
-            transition={{ duration: 1, delay: 0.2 }}
-            viewport={{ once: true }}
-            className='absolute top-0 w-full h-full z-20 bg-primary'></motion.div>
-          <motion.div
-            initial={{ scale: 1.2, y: 0 }}
-            style={{ scale: 1.2, y: y }}
-            className={'relative w-full h-full overflow-hidden '}>
-            <Image
-              className='w-full h-full object-cover '
-              src={col2}
-              alt={'a small porcelain dish with loose green tea leaves and some small cups with green tea in it'}
-              quality={10}
-            />
-          </motion.div>
-        </div>
-        <div className='relative h-[400px] min-[880px]:h-[500px] overflow-hidden rounded-3xl md:rounded-[3rem]'>
-          <motion.div
-            initial={{ height: '100%' }}
-            whileInView={{ height: '0%' }}
-            transition={{ duration: 1, delay: 0.2 }}
-            viewport={{ once: true }}
-            className='absolute top-0 w-full h-full z-20 bg-primary'></motion.div>
-          <motion.div
-            initial={{ scale: 1.2, y: 0 }}
-            style={{ scale: 1.2, y: y }}
-            className={'relative w-full h-full overflow-hidden '}>
-            <Image
-              className='w-full h-full object-cover '
-              src={col3}
-              alt={'a small dish with some water and some green leaves on top'}
-              quality={10}
-            />
-          </motion.div>
-        </div>
-      </div>
-    </Container>
+      </Container>
+    </LazyMotion>
   )
 }
 
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from 'next/image'
 import hero from '/public/hero.jpg'
-import { useScroll, useTransform, motion } from 'framer-motion'
+import { useScroll, useTransform, m, domAnimation, LazyMotion } from 'framer-motion'
 import { Container } from './container'
 export const Hero = () => {
   const { scrollYProgress } = useScroll()
@@ -9,75 +9,77 @@ export const Hero = () => {
 
   return (
     <>
-      <Container>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-          <div className='hero-text flex flex-col justify-center items-center leading-tight' id='hero'>
-            <div className='overflow-hidden'>
-              <motion.p
+      <LazyMotion features={domAnimation}>
+        <Container>
+          <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            <div className='hero-text flex flex-col justify-center items-center leading-tight' id='hero'>
+              <div className='overflow-hidden'>
+                <m.p
+                  initial={{ opacity: 0, y: '35%' }}
+                  whileInView={{ opacity: 1, y: '0%' }}
+                  transition={{ duration: 0.5, delay: 0.0 }}
+                  viewport={{ once: true }}
+                  className='font-serif text-3xl md:text-4xl mt-[9rem]'>
+                  緑茶
+                </m.p>
+              </div>
+              <div className='overflow-hidden pb-4'>
+                <m.p
+                  initial={{ opacity: 0, y: '35%' }}
+                  whileInView={{ opacity: 1, y: '0%' }}
+                  transition={{ duration: 1, delay: 0.2 }}
+                  viewport={{ once: true }}
+                  className='font-serif font-bold tracking-tighter text-5xl md:text-6xl mt-3'>
+                  Ryokucha
+                </m.p>
+              </div>
+              <div className='overflow-hidden relative mt-[-1rem]'>
+                <m.h1
+                  initial={{ opacity: 0, y: '35%' }}
+                  whileInView={{ opacity: 1, y: '0%' }}
+                  transition={{ duration: 0.5, delay: 0.4 }}
+                  viewport={{ once: true }}
+                  className='text-lg leading-snug font-sans text-center mt-6 px-2 md:px-0'>
+                  Enjoy the Finest Japanese Green Tea.
+                  <br className='hidden md:block' /> Taste the extraordinary flavour of pure, organic green{' '}
+                  <br className='hidden md:block' />
+                  {`tea from Japan's finest tea growers.`}
+                </m.h1>
+              </div>
+              <m.button
                 initial={{ opacity: 0, y: '35%' }}
                 whileInView={{ opacity: 1, y: '0%' }}
-                transition={{ duration: 1, delay: 0.0 }}
+                transition={{ duration: 0.5, delay: 0.6 }}
                 viewport={{ once: true }}
-                className='font-serif text-3xl md:text-4xl mt-[9rem]'>
-                緑茶
-              </motion.p>
+                className='py-4 px-16 text-background text-lg font-semibold rounded-full transition-colors mt-10 bg-primary shadow-2xl shadow-primary/80 hover:bg-primary/90'>
+                BROWSE OUR TEAS
+              </m.button>
             </div>
-            <div className='overflow-hidden pb-4'>
-              <motion.p
-                initial={{ opacity: 0, y: '35%' }}
-                whileInView={{ opacity: 1, y: '0%' }}
-                transition={{ duration: 1, delay: 0.2 }}
+            <div className='hero-image w-full relative  mt-14  mb-24 md:mb-32 overflow-hidden rounded-3xl md:rounded-[3rem]'>
+              <m.div
+                initial={{ height: '100%' }}
+                whileInView={{ height: '0%' }}
+                transition={{ duration: 0.5, delay: 0.8 }}
                 viewport={{ once: true }}
-                className='font-serif font-bold tracking-tighter text-5xl md:text-6xl mt-3'>
-                Ryokucha
-              </motion.p>
+                className='absolute top-0 w-full h-full z-20 bg-primary'></m.div>
+              <figure className='overflow-hidden '>
+                <m.div
+                  initial={{ scale: 1.2, y: 0 }}
+                  style={{ scale: 1.2, y: y }}
+                  className={'relative w-full h-full overflow-hidden'}>
+                  <Image
+                    src={hero}
+                    alt={'teapot, teacups filled with green tea with some loose green tea leaves around them'}
+                    className='object-cover h-[55vh] md:h-[80vh]  w-full rounded-3xl md:rounded-[3rem]'
+                    quality={10}
+                    priority
+                  />
+                </m.div>
+              </figure>
             </div>
-            <div className='overflow-hidden relative mt-[-1rem]'>
-              <motion.h1
-                initial={{ opacity: 0, y: '35%' }}
-                whileInView={{ opacity: 1, y: '0%' }}
-                transition={{ duration: 1, delay: 0.4 }}
-                viewport={{ once: true }}
-                className='text-lg leading-snug font-sans text-center mt-6 px-2 md:px-0'>
-                Enjoy the Finest Japanese Green Tea.
-                <br className='hidden md:block' /> Taste the extraordinary flavour of pure, organic green{' '}
-                <br className='hidden md:block' />
-                {`tea from Japan's finest tea growers.`}
-              </motion.h1>
-            </div>
-            <motion.button
-              initial={{ opacity: 0, y: '35%' }}
-              whileInView={{ opacity: 1, y: '0%' }}
-              transition={{ duration: 1, delay: 0.6 }}
-              viewport={{ once: true }}
-              className='py-4 px-16 text-background text-lg font-semibold rounded-full transition-colors mt-10 bg-primary shadow-2xl shadow-primary/80 hover:bg-primary/90'>
-              BROWSE OUR TEAS
-            </motion.button>
-          </div>
-          <div className='hero-image w-full relative  mt-14  mb-24 md:mb-32 overflow-hidden rounded-3xl md:rounded-[3rem]'>
-            <motion.div
-              initial={{ height: '100%' }}
-              whileInView={{ height: '0%' }}
-              transition={{ duration: 1, delay: 0.8 }}
-              viewport={{ once: true }}
-              className='absolute top-0 w-full h-full z-20 bg-primary'></motion.div>
-            <figure className='overflow-hidden '>
-              <motion.div
-                initial={{ scale: 1.2, y: 0 }}
-                style={{ scale: 1.2, y: y }}
-                className={'relative w-full h-full overflow-hidden'}>
-                <Image
-                  src={hero}
-                  alt={'teapot, teacups filled with green tea with some loose green tea leaves around them'}
-                  className='object-cover h-[55vh] md:h-[80vh]  w-full rounded-3xl md:rounded-[3rem]'
-                  quality={10}
-                  priority
-                />
-              </motion.div>
-            </figure>
-          </div>
-        </motion.div>
-      </Container>
+          </m.div>
+        </Container>
+      </LazyMotion>
     </>
   )
 }
