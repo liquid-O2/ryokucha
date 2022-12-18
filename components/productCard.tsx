7c99df3b1346aac2f97b698c68261d8d22690b96
arunava(liquido2) <this.is.arunava.b@gmail.com>
2022-12-18 05:38:31 +0530

    fix: like button initiating redirect

--- components/productCard.tsx
+++ components/productCard.tsx
@@ -46,35 +46,40 @@ const Card = ({ img, price, title, attributes, id, className }: CardProps) => {
   return (
     <>
       <LazyMotion features={loadFeatures}>
-        <m.div className={className} id={'card'} onTap={() => router.push(`/shop/${id}`)}>
-          <figure className='p-14 bg-tertiary relative rounded-3xl flex justify-center items-center  min-h-[367px]'>
-            <Image
-              src={img}
-              alt={`${title} loose tea leaf`}
-              width={300}
-              height={308}
-              className='object-cover h-full w-full pointer-events-none'
-            />
-            <button
-              disabled={!isLoggedIn}
-              className=' disabled:opacity-20 absolute top-4 right-4 text-primary'
-              onClick={() => toggleUpdateLiked()}>
-              <span className='sr-only'> like button </span>
-              <Heart className={isLiked ? 'fill-rose-500 stroke-rose-500' : ''} />
-            </button>
-          </figure>
-          <div className='card-details flex flex-col justify-center items-center mt-4'>
-            <p className=' text-xl md:text-2xl font-bold mb-2 leading-none md:leading-none'>{price}</p>
-            <p className='text-lg md:text-xl mb-3 leading-none'>{title}</p>
-            <div className='flex justify-between items-center gap-2 mb-4'>
-              {attributes.map((attr, index) => (
-                <p key={index} className='px-4 py-1 max-w-fit rounded-full border border-primary border-opacity-50 '>
-                  {attr}
-                </p>
-              ))}
+        <div className='relative'>
+          <button
+            disabled={!isLoggedIn}
+            className=' disabled:opacity-20 absolute z-20 top-4  right-8 text-primary'
+            onClick={(e) => {
+              e.stopPropagation()
+              toggleUpdateLiked()
+            }}>
+            <span className='sr-only'> like button </span>
+            <Heart className={isLiked ? 'fill-rose-500 stroke-rose-500' : ''} />
+          </button>
+          <m.div className={className} id={'card'} onTap={() => router.push(`/shop/${id}`)}>
+            <figure className='p-14 bg-tertiary relative rounded-3xl flex justify-center items-center  min-h-[367px]'>
+              <Image
+                src={img}
+                alt={`${title} loose tea leaf`}
+                width={300}
+                height={308}
+                className='object-cover h-full w-full pointer-events-none'
+              />
+            </figure>
+            <div className='card-details flex flex-col justify-center items-center mt-4'>
+              <p className=' text-xl md:text-2xl font-bold mb-2 leading-none md:leading-none'>{price}</p>
+              <p className='text-lg md:text-xl mb-3 leading-none'>{title}</p>
+              <div className='flex justify-between items-center gap-2 mb-4'>
+                {attributes.map((attr, index) => (
+                  <p key={index} className='px-4 py-1 max-w-fit rounded-full border border-primary border-opacity-50 '>
+                    {attr}
+                  </p>
+                ))}
+              </div>
             </div>
-          </div>
-        </m.div>
+          </m.div>
+        </div>
       </LazyMotion>
     </>
   )
