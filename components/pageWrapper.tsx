8d620d2f78729456c6f8ec9087fbe08b51ee2e16
liquidO2 <this.is.arunava.b@gmail.com>
2022-12-26 23:54:32 +0530

    update: lazy load framer motion

--- components/pageWrapper.tsx
+++ components/pageWrapper.tsx
@@ -1,19 +1,22 @@
 'use client'
 
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence, LazyMotion, m } from 'framer-motion'
 import { ReactNode } from 'react'
 
 export const PageWrapper = ({ children }: { children: ReactNode }) => {
+  const loadFeatures = () => import('../components/utils/framerFeaturesMax').then((res) => res.default)
   return (
-    <AnimatePresence>
-      <motion.div
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 10 }}
-        transition={{ duration: 0.4, ease: 'easeOut' }}>
-        {children}
-      </motion.div>
-    </AnimatePresence>
+    <LazyMotion features={loadFeatures}>
+      <AnimatePresence>
+        <m.div
+          initial={{ opacity: 0, y: 10 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 10 }}
+          transition={{ duration: 0.4, ease: 'easeOut' }}>
+          {children}
+        </m.div>
+      </AnimatePresence>
+    </LazyMotion>
   )
 }
 
