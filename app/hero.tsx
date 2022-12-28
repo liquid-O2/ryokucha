ef7fc8b14ae60b9b3949e308a500e55a5ba447d9
liquidO2 <this.is.arunava.b@gmail.com>
2022-12-28 09:18:00 +0530

    test

--- app/hero.tsx
+++ app/hero.tsx
@@ -7,18 +7,19 @@ import SlideUp from '../components/slideUp'
 export const Hero = () => {
   return (
     <Section full className='h-[99vh] md:h-[99vh] text-background'>
-      <section className='relative w-full h-full '>
-        <Image
-          src={hero}
-          fill
-          placeholder='blur'
-          sizes='(min-width: 100px) 100vw'
-          alt={' a chawan(tea bowl) on top of a a wooden table with a hand holding a chasen(bamboo whisk) over it'}
-          quality={75}
-          className='object-cover h-full w-full'
-          priority={true}
-        />
-        <section className='relative z-20 w-full h-full  flex flex-col '>
+      <section className=' w-full h-full '>
+        <figure className='relative z-[1]'>
+          <Image
+            src={hero}
+            sizes='(min-width: 100px) 100vw'
+            alt={' a chawan(tea bowl) on top of a a wooden table with a hand holding a chasen(bamboo whisk) over it'}
+            quality={75}
+            className='object-cover relative h-full w-full'
+            priority={true}
+          />
+        </figure>
+
+        <section className='relative z-30 w-full h-full  flex flex-col '>
           <Container className='self-end flex flex-col justify-end mt-auto h-full'>
             <SlideUp delay={0.6}>
               <div className='mb-24 md:mb-32'>
