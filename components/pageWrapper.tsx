2dc4827426560d6f97d0d039b3799ec79a661968
liquidO2 <this.is.arunava.b@gmail.com>
2022-12-29 04:01:03 +0530

    update: reduce framer motion import size

--- components/pageWrapper.tsx
+++ components/pageWrapper.tsx
@@ -1,24 +1,23 @@
-// 'use client'
+'use client'
 
-// import { AnimatePresence, LazyMotion, m } from 'framer-motion'
+import { AnimatePresence, LazyMotion, m } from 'framer-motion'
 import { ReactNode } from 'react'
 
 export const PageWrapper = ({ children }: { children: ReactNode }) => {
-  // const loadFeatures = () => import('../components/utils/framerFeaturesMax').then((res) => res.default)
-  // return (
-  //   <LazyMotion features={loadFeatures}>
-  //     <AnimatePresence>
-  //       <m.div
-  //         initial={{ opacity: 0, y: 10 }}
-  //         animate={{ opacity: 1, y: 0 }}
-  //         exit={{ opacity: 0, y: 10 }}
-  //         transition={{ duration: 0.3 }}>
-  //         {children}
-  //       </m.div>
-  //     </AnimatePresence>
-  //   </LazyMotion>
-  // )
-  return <> {children}</>
+  const loadFeatures = () => import('../components/utils/framerFeatures').then((res) => res.default)
+  return (
+    <LazyMotion features={loadFeatures}>
+      <AnimatePresence>
+        <m.div
+          initial={{ opacity: 0, y: 5 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 5 }}
+          transition={{ duration: 0.3 }}>
+          {children}
+        </m.div>
+      </AnimatePresence>
+    </LazyMotion>
+  )
 }
 
 export default PageWrapper
