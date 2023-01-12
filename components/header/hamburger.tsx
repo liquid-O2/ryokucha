bb80d143e41e39934a5f12777700263cce58df7b
liquido2 <this.is.arunava.b@gmail.com>
2023-01-12 16:43:19 +0530

    fix: hamburger icon not filled

--- components/header/hamburger.tsx
+++ components/header/hamburger.tsx
@@ -22,22 +22,22 @@ const HamburgerMenu = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
         >
           <span className='sr-only'>hamburger menu button</span>
           <m.span
-            style={{ color, borderColor: color }}
+            style={{ color, borderColor: color, backgroundColor: color }}
             className={`absolute mb-1 h-[2px] w-[24px] origin-center rounded-[10px]   transition-transform  ease-in ${
               hamburgerOpen ? ' top-[50%] rotate-[135deg] ' : 'top-[35%]'
-            } border `}
+            } border bg-current `}
           ></m.span>
           <m.span
-            style={{ color, borderColor: color }}
+            style={{ color, borderColor: color, backgroundColor: color }}
             className={` ${
               hamburgerOpen && 'opacity-0'
-            } absolute  top-[50%] mb-1 h-[2px] w-[24px] origin-center rounded-[10px] border transition-transform  ease-in `}
+            } absolute  top-[50%] mb-1 h-[2px] w-[24px] origin-center rounded-[10px] border bg-current transition-transform ease-in `}
           ></m.span>
           <m.span
-            style={{ color, borderColor: color }}
+            style={{ color, borderColor: color, backgroundColor: color }}
             className={`absolute  h-[2px] w-[24px] origin-center transition-transform  ease-in ${
               hamburgerOpen ? 'top-[50%] -rotate-[135deg] ' : 'top-[65%]'
-            } rounded-[10px] border    `}
+            } rounded-[10px] border bg-current   `}
           ></m.span>
         </button>
         <div className=' absolute top-full right-0 z-50 mt-2 min-w-[300px]  overflow-hidden text-primary'>
