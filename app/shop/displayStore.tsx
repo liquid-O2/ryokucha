d70af0f900e5851a6d94d0930e561e7bfb3c7253
liquidO2 <this.is.arunava.b@gmail.com>
2023-01-14 20:18:16 +0530

    update: add more semantic tags

--- app/shop/displayStore.tsx
+++ app/shop/displayStore.tsx
@@ -24,19 +24,19 @@ const DisplayStore = ({ fetchedTeas }: { fetchedTeas: Teas[] }) => {
 
   return (
     <section className='flex flex-col items-center justify-center'>
-      <div className='input-wrapper mt-8 mb-16  w-[70%] max-[1100px]:w-[99%]'>
+      <figure className='input-wrapper mt-8 mb-16  w-[70%] max-[1100px]:w-[99%]'>
         <input
           onChange={(e) => handleSearch(e.target.value)}
           type='text'
           placeholder='Search for your favourite tea'
           className='h-14  w-full rounded-full border border-primary/10 bg-tertiary-light bg-opacity-[0.01] py-3 pr-4 pl-[56px] text-primary'
         />
-        <div className='icon pointer-events-none absolute left-[1.5rem] top-[1.2rem]'>
+        <picture className='icon pointer-events-none absolute left-[1.5rem] top-[1.2rem]'>
           <Search size={20} className={`stroke-primary/70`} />
-        </div>
-      </div>
+        </picture>
+      </figure>
 
-      <div className='relative w-full'>
+      <section className='relative w-full'>
         <SortBtns setTeas={setTeas} />
         <div className='grid grid-cols-4 gap-x-4  gap-y-12 pb-32 pt-8 max-[1200px]:grid-cols-3 max-[910px]:grid-cols-2 max-[612px]:grid-cols-1  lg:gap-x-6'>
           {(!searchResult || searchResult.length === 0) &&
@@ -54,7 +54,7 @@ const DisplayStore = ({ fetchedTeas }: { fetchedTeas: Teas[] }) => {
               )
             })}
         </div>
-      </div>
+      </section>
     </section>
   )
 }
